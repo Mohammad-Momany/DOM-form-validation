3c4ad89573fce880dfb78a66479f76ff14cd46fe
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2021-01-24 21:11:20 +0200

    Create checkEmail and checkRequired function

---
+++ script.js
@@ -0,0 +1,45 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+
+// Show input error message
+const showError = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+};
+
+// Show success outline
+const showSuccess = (input) => {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+};
+
+// Check email is valid
+const checkEmail = (input) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
+};
+
+// Check required fields
+const checkRequired = (inputArr) => {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkRequired([username, email, password, confirmPassword]);
+});
